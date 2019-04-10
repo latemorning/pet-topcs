03290e923b1a7117d664945dca499cd40acdc27c
harry <latemorning@gmail.com>
2019-04-10 15:37:32 +0900

    first commit

---
+++ src/main/java/com/pet/topcs/TopcsApplication.java
@@ -0,0 +1,13 @@
+package com.pet.topcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopcsApplication.class, args);
+	}
+
+}
